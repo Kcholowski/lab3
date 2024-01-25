ef1ef9779f841a02c5cb7ba7efdd466c2dae57e8
Kcholowski <cholowskik@mymacewan.ca>
2024-01-25 13:34:57 -0700

    Make monster search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
+import { SearchBar } from "./components/searchbar/searchbar.component";
+import { CardList } from "./components/cardlist/cardlist.component";
+import axios from 'axios';
 
 function App() {
+  const [monsters, setMonsters] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [filteredMonsters, setFilteredMonsters] = useState([]);
+
+  useEffect(() => {
+    const fetchMonsters = async () => {
+      // const response = await fetch("https://jsonplaceholder.typicode.com/users");
+      // const users = await response.json();
+      const response = await axios(
+        'https://jsonplaceholder.typicode.com/users',
+      );
+      setMonsters(response.data);
+    };
+
+    fetchMonsters();
+  }, []);
+
+  useEffect(() => {
+    let filtered = [];
+    if(searchInput === ""){
+      filtered = monsters;
+    } else {
+      filtered = monsters.filter(monster =>
+        monster.name.toLowerCase().includes(searchInput.toLowerCase())
+      );
+    }
+    setFilteredMonsters(filtered);
+  }, [monsters, searchInput]);
+
+  const handleInput = e => {
+    console.log(e.target.value);
+    setSearchInput(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Monster Rolodex</h1>
+      <SearchBar
+        placeholder='Search Monster'
+        handleInput={handleInput}
+      />
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 }
